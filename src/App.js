cedee681d520ec5555529d7f054051d6326e719c
Uma Nagarjuna Banka <ubanka@guidewire.com>
2021-11-22 00:47:43 -0500

    Get All the Movies and Build Rows

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from 'react';
+import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
     //We need snippet of code which runs based on a specific condition/variable
-    useEffect(()=>{
+    useEffect(() => {
 
-        async function fetchdata(){
+        async function fetchdata() {
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
             console.log(movies);
@@ -18,18 +19,23 @@ function Row({title, fetchUrl}) {
 
         fetchdata();
 
-        }, [fetchUrl]); 
+    }, [fetchUrl]);
     //if[], run once when the Row loads, and don't run again; if[fetchUrl], dependent on the fetchUrl, it will run everytime fetchUrl changes
 
     return (
         <div className="row">
-            <h2>{title}</h2>    
-            <div className="row__posters"> 
+            <h2>{title}</h2>
+            <div className="row__posters">
                 {
-                movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                    movies.map(movie => (
+                        <img
+                            key={movie.id}
+                            className="row__poster"
+                            src={`${base_url}${movie.poster_path}`}
+                            alt={movie.name}
+                        />
                     ))
-                }      
+                }
             </div>
         </div>
         //"movie.poster_path gives "/apbrbWs8M9lyJYU5WXrpFBKL1z.jpg"
